067857ba21d9b5e0ff8cb74b2f58589cc6991145
ReetMhaske <85624612+ReetMhaske@users.noreply.github.com>
2021-11-22 19:20:35 +0530

    multiplicity distribution fitting, 2D, contour, final

--- mult_2d.C
+++ mult_2d.C
@@ -1,68 +1,103 @@
 void mult_2d()
 {
   TFile *f=new TFile("13TeV_CR0_RHoff.root");
-  TTree *tree = (TTree*)f->Get("pytree"); // use the required tree here
+  TTree *tree = (TTree*)f->Get("pytree80100;6"); // use the required tree here
    Int_t entries = tree->GetEntries();
 
  const Int_t maxTrack=10000;
    
  Int_t ntrack = 0;
    Double_t pT[maxTrack];
-  Double_t eta[maxTrack];
+ Int_t pid[maxTrack];
   Double_t rap[maxTrack];
-  Double_t phi[maxTrack];
+  Double_t eta[maxTrack];
 
 
   tree->SetBranchAddress("ntrack",&ntrack);
-  tree->SetBranchAddress("phi",&phi);
-  tree->SetBranchAddress("pT",&pT);
   tree->SetBranchAddress("eta",&eta);
+  tree->SetBranchAddress("pT",&pT);
+  tree->SetBranchAddress("pid",&pid);
  
-   gStyle->SetPalette(52);
-    gStyle->SetOptStat(0);
-   gStyle->SetOptTitle(0);
-  TH1D *hmult_acc = new TH1D("hmul_acc","No. of events vs accepted multiplicity;Multiplicity in accepted region; N_{events}",50,0,50);
-  TH1D *hmult_notacc = new TH1D("hmul_notacc","No. of events vs non-accepted multiplicity;Multiplicity in accepted region; N_{events}",50,0,50);
-  TH1D *heta = new TH1D("heta","Rapidity",100,-10.0,10.0);
-  TH2D *mult_2=new TH2D("mult_2","2D Histogram; Accepted Multiplicity; Non-Accepted Multiplicity; No. of Events",
-                         50,0,50,
-                         50,0,50); 
+   gStyle->SetPalette(1);
+    gStyle->SetOptStat("emr");
+   gStyle->SetOptTitle(2);
+ char title[]= "Multiplicity Distribution for bin beyond 100";
+max_mult= tree->GetMaximum("ntrack");
+  auto TH1D *hmult_acc = new TH1D("hmul_acc",title,100,0,100);
+ TAxis *X1=hmult_acc->GetXaxis();
+   X1->SetTitle("Multiplicity w.r.t |eta|>1.5");  X1->SetTitleOffset(1);
 
-  for(Int_t ii=0; ii<entries; ii++)  {  //  A Collison Event loop starts here
+ TAxis *Y1=hmult_acc->GetYaxis();
+   Y1->SetTitle("Number of events");
+  Y1->SetTitleOffset(1);
+
+
+ auto TH1D *hmult_notacc = new TH1D("hmul_notacc",title,100,0,100);
+  TAxis *X2=hmult_notacc->GetXaxis();
+ X2->SetTitle("Multiplicity w.r.t paticles with |eta|<1 and pT >0.05");
+X2->SetTitleOffset(1);  
+
+
+ TAxis *Y2=hmult_notacc->GetYaxis();
+   Y2->SetTitle("Number of events");
+  Y2->SetTitleOffset(1);
+  
+  
+auto TH2D *mult_2=new TH2D("mult_2",title,
+                         100,0,100,
+                         100,0,100); 
+
+TAxis *X=mult_2->GetXaxis();
+ X->SetTitle("Multiplicity w.r.t particles with |eta|> 1.5");
+X->SetTitleOffset(1.4);
+
+
+ TAxis *Y=mult_2->GetYaxis();
+   Y->SetTitle("Multiplicity w.r.t particles with |eta|<1 and pT>0.05");
+  Y->SetTitleOffset(1.4);
+   
+TAxis *Z=mult_2->GetZaxis();
+   Z->SetTitle("Number of Events");
+  Z->SetTitleOffset(1.4);
+ 
+ for(Int_t ii=0; ii<entries; ii++)  {  //  A Collison Event loop starts here
     tree->GetEntry(ii);
 
       Int_t ntrks = ntrack;
-
-
-     
-   Int_t new_mult=0;
-      for(int i=0; i<ntrks; i++) { //track loop is here loop on no. of particels released in the particular event
-
-     if( (eta[i]>1.5) || (eta[i]<-1.5)  ){
-       new_mult++;};
-    // Double_t eta1 = eta[i];
-    // heta->Fill(eta1);
-
-
-   }
+      Int_t new_mult=0,analysis=0;
+      for(Int_t i=0;i<ntrks;i++){  
+       	if(abs(eta[i])<1 && pT[i]>0.05){analysis++;} 
+       	if(abs(eta[i])>1.5){new_mult++;}
+                                  }
  hmult_acc->Fill(new_mult);
- hmult_notacc->Fill(ntrks-new_mult);
- mult_2->Fill(new_mult,ntrks-new_mult); 
+ hmult_notacc->Fill(analysis);
+  mult_2->Fill(new_mult,analysis); 
 if (ii%500000==0){cout<< ii<<"\n";}
  }
+
+TF1*poisson=new TF1("Poisson","[0]*(TMath::Poisson(x,[1]))",0,100);
+poisson->SetParNames("normalization constnt","lamda");
+Double_t mean=hmult_acc->GetMean();
+poisson->SetParameters(entries,hmult_acc->GetMean());
  auto c1=new TCanvas("Canvas1","Canvas1",1000,1000);
- c1->Divide(1,2);
+hmult_acc->Fit("Poisson") ;
+gStyle->SetOptFit(0000);
+hmult_acc->Draw("BOX ");
+//hmult_acc->Draw("SAME C ");
+
+auto c2= new TCanvas("canvas2","canvas2",1000,1000);
+poisson->SetParameters(entries,hmult_acc->GetMean());
+hmult_notacc->Fit("Poisson");
+gStyle->SetOptFit(0000);
+hmult_notacc->Draw(" BOX ");
+//hmult_notacc->Draw("SAME C");   
+auto c3=new TCanvas("Canvas3","Canvas3",1000,1000);
+mult_2->Draw("Lego2");
+auto c4=new TCanvas("Canvas4","Canvas4",1000,1000);
+mult_2->Draw("ContZ");
+
+
  
-  //heta->Draw();
-c1->cd(1);   hmult_acc->Draw();
-c1->cd(2); hmult_notacc->Draw();
-    
-auto c2=new TCanvas("Canvas2","Canvas2",1000,1000);
-c2->Divide(2,2);
-c2->cd(1);mult_2->Draw("Lego2");
-c2->cd(2);mult_2->Draw("Colz");
-c2->cd(3);mult_2->Draw("Cont1");
-c2->cd(4);mult_2->Draw("Surf3");
 cout << "Moments of Distribution:\n"
          << " - Mean     = " << hmult_acc->GetMean() << " +- "
                              << hmult_acc->GetMeanError() << "\n"
@@ -70,4 +105,8 @@ cout << "Moments of Distribution:\n"
                              <<hmult_acc->GetStdDevError() << "\n"
          << " - Skewness = " << hmult_acc->GetSkewness() << "\n"
          << " - Kurtosis = " << hmult_acc->GetKurtosis() << "\n";
+
+
+
+
 }
